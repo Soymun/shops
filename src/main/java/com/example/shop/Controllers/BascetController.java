11d4f8e5e0c0f828d3a5db71058ac9c401473d40
pkekepke@mail.ru <Rjnvehpbr1+>
2022-08-19 20:31:05 +0300

    add Basket controller imp.

--- src/main/java/com/example/shop/Controllers/BascetController.java
+++
@@ -1,4 +0,0 @@
-package com.example.shop.Controllers;
-
-public class BascetController {
-}
---
+++ src/main/java/com/example/shop/DTO/ProductBasketDTO.java
@@ -0,0 +1,11 @@
+package com.example.shop.DTO;
+
+import lombok.Data;
+
+@Data
+public class ProductBasketDTO {
+
+    private Long userId;
+
+    private Long productId;
+}
