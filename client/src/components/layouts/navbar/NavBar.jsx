17f00e17893c40e2861c9edf1c5643882341bb54
ruslanchik <rak.geter@gmail.com>
2023-03-07 16:05:33 +0300

    add map and review

--- client/src/components/layouts/navbar/NavBar.jsx
+++ client/src/components/layouts/navbar/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from './NavBar.module.css'
+import {NavLink} from "react-router-dom";
 
 const NavBar = ({setActive, active}) => {
     return (
@@ -7,12 +8,12 @@ const NavBar = ({setActive, active}) => {
             <div className={classes.wrapper}>
                 <div onClick={e => e.stopPropagation()} className={classes.container}>
                     <div className={classes.titles}>
-                        <a href='' className={classes.menuItem}>Меню</a>
-                        <a href='' className={classes.menuItem}>Купоны</a>
-                        <a href='' className={classes.menuItem}>King Club</a>
-                        <a href='' className={classes.menuItem}>Работа</a>
-                        <a href='' className={classes.menuItem}>FAQ</a>
-                        <a href='' className={classes.menuItem}>Контакты</a>
+                        <NavLink to={'/'} className={classes.menuItem}>Меню</NavLink>
+                        <NavLink to={'/'} className={classes.menuItem}>Купоны</NavLink>
+                        <NavLink to={'/king-club'} className={classes.menuItem}>King Club</NavLink>
+                        <NavLink to={'/'} className={classes.menuItem}>Работа</NavLink>
+                        <NavLink to={'/'} className={classes.menuItem}>FAQ</NavLink>
+                        <NavLink to={'/'} className={classes.menuItem}>Контакты</NavLink>
                     </div>
                     <div className={classes.subtitles}>
                         <a className={classes.menuSmallItem} href=''>Новинки и акции</a>
